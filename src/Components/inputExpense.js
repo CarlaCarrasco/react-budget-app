26f7f0bf823b9798af492e5324b083aca49eb226
CarlaCarrasco <carlacarr37@gmail.com>
2021-03-24 06:07:57 -0500

    added input place holder and made css changes

--- src/Components/inputExpense.js
+++ src/Components/inputExpense.js
@@ -3,6 +3,6 @@ import React from 'react';
 export const InputExpense = props => {
     return (
     
-        <input type="text" name="expense"/>
+        <input type="text" name="expense" placeholder="Add expense..."/>
     );
 }
