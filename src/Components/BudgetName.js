0dd732440e2f7c986fd4aa839b4b327478638855
CarlaCarrasco <carlacarr37@gmail.com>
2021-05-09 08:05:33 -0500

    added edit BudgetName textFeild

--- src/Components/BudgetName.js
+++ src/Components/BudgetName.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import EditIcon from '@material-ui/icons/Edit';
+import AddIcon from '@material-ui/icons/Add';
+
 
 const useStyles = makeStyles((theme) => ({
   h2: {
@@ -16,20 +18,74 @@ const useStyles = makeStyles((theme) => ({
     margin: '5px 0 0 10px',
     color: '#5f5f5f',
     fontSize: '1.2rem'
+  },
+  textField: {
+    maxWidth: 500,
   }
 }));
 
 export const BudgetName = props => {
    const classes = useStyles();
+   const [edit, setEdit] = useState(false);
+   const editBudgetName = false;
+  //  const edit = () => { 
+  //   if (editBudgetName) {
+  //     editBudgetName = false;
+  //   }
+  //   else {
+  //     editBudgetName = true;
+  //   }
 
-    return (
+  //   console.log(editBudgetName)
+  //  }
+
+  // onClick h2 display none & TextFeild display
+  // Onclick (again) TextFeild display none h2 display
+  // Toggle if not click false else true
+    if(edit) {
+      return(
+        <div className={classes.flex}>
+          <TextField className={classes.textField} 
+          fullWidth id="standard-basic" 
+          name="expense" label="Add expense..."
+          />
+          <AddIcon className={classes.editIcon} 
+            onClick={()=>{
+            if (edit) {
+              setEdit(false);
+              console.log(edit);
+            }
+            else {
+              setEdit(true);
+              console.log(edit);
+            }
+            }}/>
+          </div>
+      );
+    }
+    else {
+      return(
+          <div className={classes.flex}>
+          <h2 className={classes.h2}>D.I.Y Peloton</h2>  
+          <EditIcon className={classes.editIcon} 
+          onClick={()=>{
+            if (edit) {
+              setEdit(false);
+              console.log(edit);
+            }
+            else {
+              setEdit(true);
+              console.log(edit);
+            }
+          }}/>
+        </div>
+      );
+    }
+
         // <form className={classes.root} noValidate autoComplete="off">
         //     <TextField fullWidth id="filled-basic" name="expense" label="Add expense..." variant="filled" />
         // </form>
-        <div className={classes.flex}>
-          <h2 className={classes.h2}>D.I.Y Peloton</h2>
-          <EditIcon className={classes.editIcon} />
-        </div>
-        
-    );
+          
+          
+
 }
--- src/Components/inputExpense.js
+++ src/Components/inputExpense.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import AddIcon from '@material-ui/icons/Add';
