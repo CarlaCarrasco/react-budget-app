2b5ea7621013c956ee46d31251ad4944bfd728f1
CarlaCarrasco <carlacarr37@gmail.com>
2021-05-10 05:28:26 -0500

    call editClicked when 'Enter' pressed while TextField selected

--- src/Components/BudgetName.js
+++ src/Components/BudgetName.js
@@ -4,7 +4,6 @@ import TextField from '@material-ui/core/TextField';
 import EditIcon from '@material-ui/icons/Edit';
 import AddIcon from '@material-ui/icons/Add';
 
-
 const useStyles = makeStyles((theme) => ({
   h2: {
     marginTop: '0'
@@ -27,17 +26,17 @@ const useStyles = makeStyles((theme) => ({
 export const BudgetName = props => {
    const classes = useStyles();
    const [edit, setEdit] = useState(false);
+   const [name, setName] = useState('D.I.Y Peloton');
    const editClicked = () => edit ? setEdit(false) : setEdit(true);
 
-  // onClick h2 display none & TextFeild display
-  // Onclick (again) TextFeild display none h2 display
-  // Toggle if not click false else true
     if(edit) {
       return(
         <div className={classes.flex}>
           <TextField className={classes.textField} 
           fullWidth id="standard-basic" 
           name="expense" label="Add expense..."
+          onChange={(e) => setName(e.target.value)}
+          onKeyPress={(e) => {if(e.key === 'Enter') editClicked()}}          
           />
           <AddIcon className={classes.editIcon} 
             onClick={editClicked}/>
@@ -47,17 +46,14 @@ export const BudgetName = props => {
     else {
       return(
           <div className={classes.flex}>
-          <h2 className={classes.h2}>D.I.Y Peloton</h2>  
+          <h2 className={classes.h2}>{name}</h2>  
           <EditIcon className={classes.editIcon} 
           onClick={editClicked}/>
         </div>
       );
     }
-
         // <form className={classes.root} noValidate autoComplete="off">
         //     <TextField fullWidth id="filled-basic" name="expense" label="Add expense..." variant="filled" />
         // </form>
           
-          
-
 }
